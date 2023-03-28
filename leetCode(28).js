e688a57072eb6b47d9fe3771b830fb5710aa4a27
Rishi Kant <126663254+Rishi-kant@users.noreply.github.com>
2023-03-28 17:36:04 +0530

    Add files via upload

---
+++ leetCode(28).js
@@ -0,0 +1,7 @@
+
+
+var strStr = function(haystack, needle) {
+    let value=haystack.toLowerCase().indexOf(needle.toLowerCase())
+    return value
+};
+console.log(haystack = "sadbutsad", needle = "sad")
